8632044a0de6de6a0383db305db0c42175e9d3ee
karimliaghabala <agabala.93@gmail.com>
2023-10-02 03:16:25 +0400

    added header

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+const MenuItem = ({menu,index}) => {
+  const [isHoverred, setIsHoverred] = useState(false);
+  return (
+  <a href={menu.uri} className=" w-12 h-12 rounded-full flex items-center justify-center group cursor-pointer hover:bg-gradient-to-br hover:from-primary hover:to-secondary relative"
+  onMouseEnter={()=> setIsHoverred(true)}
+  onMouseLeave={()=> setIsHoverred(false)}
+  >
+    <menu.Icon className={`text-texlight group-hover:text-bgPrimary text-xl`} />
+    {/* tool tip */}
+    <AnimatePresence>
+      {isHoverred && (
+    <motion.div 
+    initial={{opacity : 0, x: -25}}
+    animate={{opacity : 1, x: 0}}
+    exit={{opacity : 0, x: -25}}
+    className="absolute bg-white rounded-md px-6 py-2 -left-[140px] after:absolute after:-right-1 after:top-3 after:w-3 after:h-3 after:bg-white after:rotate-45"
+    style={{boxShadow : "inset 0px 0px 10px rgba(0,0,0,0.4)"}}
+    >
+    <p className="text-bgPrimary">{menu?.name}</p>
+    </motion.div>
+      )}
+    </AnimatePresence>
+
+  </a>
+  )
 };
 
 export default MenuItem;
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import {Menus} from "../utils/helper"
+import {MenuItem} from "../components"
 
 const Header = () => {
   return (
-    <div className="text-white">
-      <p className="text-white">
-      Header
-      </p>
+    <div className="fixed bottom-0 right-0 lg:top-0 w-full h-auto lg:h-screen lg:w-24 flex justify-content items-end lg:items-center pb-8 lg:pb-0 z-50 ">
+      <div className="px-4 py-3 lg:py-12 rounded-full border border-[rgba(255,255,255,0.3)] flex flex-row lg:flex-col items-center justify-center gap-12 duration-200 backdrop-blur-md ">
+        {Menus && 
+          Menus.map((item,index) =>(
+          <MenuItem key={index} menu={item} index={index} />
+        ))}
+      </div>
     </div>
   );
 };
