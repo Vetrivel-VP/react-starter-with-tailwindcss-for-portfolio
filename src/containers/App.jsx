32b52e9f4fb8955d9c28417c9746b5bb6534fa57
karimliaghabala <agabala.93@gmail.com>
2023-10-02 03:21:01 +0400

    update

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,7 +3,7 @@ import {Header} from "./"
 
 const App = () => {
   return (
-    <div className="w-full xl:w-[1600px] py-32 px-4 lg:px-12 pr-4 lg:pr-32">
+    <div className="w-full xl:max-w-[1600px] py-32 px-4 lg:px-12 pr-4 lg:pr-32">
       {/*particles container */}
       {/*header */}
       <Header />
