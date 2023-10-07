be47c3fbf89aa4a77a7618daa0a5d62af7602c88
karimliaghabala <agabala.93@gmail.com>
2023-10-07 15:39:13 +0400

    update

--- src/containers/ServiceCount.jsx
+++ src/containers/ServiceCount.jsx
@@ -6,7 +6,7 @@ import {useTranslation } from 'react-i18next'
 const ServiceCount = () => {
   const {t} = useTranslation()
   return (
-  <div className="w-full py-6 lg:py-16 mt-24 flex items-center justify-center flex-wrap gap-8 pl-2">
+  <div className="w-full py-6 lg:py-16 mt-24 flex items-center justify-center flex-wrap gap-8 pl-6">
     <ServiceCard count={"1M+"} text={t('hapS')} />
     <ServiceCard count={"50+"} text={t('hapC')}  />
     <ServiceCard count={"50+"} text={t('fsProject')} />
